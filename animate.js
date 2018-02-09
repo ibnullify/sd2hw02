ef698b1739f2218658e9bc4337f38b71af431f59
Ibnul Jahan <ijahan1@stuy.edu>
2018-02-09 08:10:53 -0500

    updates

--- animate.js
+++ animate.js
@@ -1,21 +1,33 @@
 //Ibnul Jahan
 //SoftDev2 pd07
-//K01 -- They lock us in the tower whenever we get caught                       //2018-02-08
+//K03 -- They lock us in the tower whenever we get caught                   //2018-02-09
 
+
+//-----------Variables for Sizing
 var frameNum;//Most recent animation frame ID
 var growing;//Boolean to tell whether the ball is in growing or shrinking mode
 var radius;//The current radius to draw the circle with
 var maxRad;//The maximum radius to grow to
 var minRad;//The minimum radius to shrink to
 
+//-----------Variables for Bouncing
+var xcor;//X-coordinate
+var ycor;//Y-coordinate 
+var dx;//Speed in the x direction
+var dy;//Speed in the y direction
+var bounceRad;//Radius for the bouncing ball
+
 //Get the console, and get the context
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
 
 //Get the two buttons from the DOM and set their onclick functions
-//When the start button is pressed, start the animation
-var start = document.getElementById("start");
-start.addEventListener("click", function(){startAnimation();});
+//When the size button is pressed, start the animation
+var size = document.getElementById("size");
+size.addEventListener("click", function(){startSizeAnimation();});
+//When the bounce button is pressed, start the animation
+var bounce = document.getElementById("bounce");
+bounce.addEventListener("click", function(){startBounceAnimation();});
 //When the stop button is pressed, stop the animation
 var stop = document.getElementById("stop");
 stop.addEventListener("click", function(){stopAnimation();});
@@ -23,12 +35,19 @@ stop.addEventListener("click", function(){stopAnimation();});
 
 //Staying in line with the Processing comparison made in class
 var setup = function(e){
-    //Initializes variable
+    //Initializes variables for sizing
     frameNum = -1;
     growing = true;
     radius = 10;
     maxRad = 100;
     minRad = 0;
+
+    //Initializes variables for bouncing
+    dx = Math.floor(Math.random() * 5);
+    dy = Math.floor(Math.random() * 5);
+    xcor = 300;
+    ycor = 300;
+    console.log("Inital dx,dy: " + dx + "," + dy);
 
     //Draws first circle
     ctx.beginPath();
@@ -41,10 +60,19 @@ var setup = function(e){
 }
 
 //Sets up the variables and starts the animation
-var startAnimation = function(){
+var startSizeAnimation = function(){
     setup();
+    size.disabled = true;
     window.requestAnimationFrame(setSize);
 }
+
+//Sets up the variables and starts the animation
+var startBounceAnimation = function(){
+    setup();
+    bounce.disabled = true;
+    window.requestAnimationFrame(setBounce);
+}
+
 //Throws out the frame and stops the animation
 var stopAnimation = function(e){
     window.cancelAnimationFrame(frameNum);
@@ -63,7 +91,7 @@ var setSize = function(e){
 
     //Draws circle at the new size
     ctx.beginPath();
-    ctx.arc(300, 300, radius, 0, Math.PI * 2);
+    ctx.arc(xcor, ycor, radius, 0, Math.PI * 2);
     //The fillStyle is set in the grow and shrink functions
     ctx.fill();
     ctx.stroke();
@@ -99,10 +127,47 @@ var shrink = function(){
     }
 }
 
+//Draws the ball at a new position based on the speed
+var setBounce = function(e){
+    clear(e);
+    move();
+    console.log("Coordinate: " + xcor + "," + ycor);
+    
+    //Draws circle at the new coordinates
+    ctx.beginPath();
+    ctx.arc(xcor, ycor, radius, 0, Math.PI * 2);
+    //The fillStyle is set in the move function
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+    //recursive call below to make frames chain
+    frameNum = window.requestAnimationFrame(setBounce);
+    console.log("This is the current Frame ID: " + frameNum);
+
+}
+
+//Checks if the ball needs to be redirected or bounced
+var move = function(e){
+    //Boundaries for x are 0 and c.width
+    //Boundaries for y are 0 and c.height
+    if (xcor - radius + dx <= 0 || xcor + radius + dx >= c.width) {
+	dx*=-1;
+	ctx.fillStyle = 'orange';
+    }
+    if (ycor - radius + dy <= 0 || ycor + radius + dy >= c.height) {
+	dy*=-1;
+	ctx.fillStyle = 'pink';
+    }
+    xcor+=dx;
+    ycor+=dy;
+
+}
+
 //Throws out the last frame of the animation
 //frameNum should be the most recent result of window.requestAnimationFrame
 var stopAnimation = function(e){
     window.cancelAnimationFrame(frameNum);
+    window.cancelAnimationFrame(frameNum-1);
 }
 
 //Used in between frames to prevent that streaking tail
